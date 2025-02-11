90e6ea3ab092380ba59c21ed51e0cfd01b07856d
SABBIR <absabbir1@gmail.com>
2025-02-11 10:51:56 +0600

    Scheduled Driver Deactivation Done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\PassengerMiddleware;
 use App\Http\Middleware\PilotMiddleware;
 use App\Http\Middleware\SubAdminMiddleware;
 use App\Http\Middleware\SuperAdminMiddleware;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -30,4 +31,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })->booted(function () { // Add this block
+        $schedule = app(Schedule::class);
+
+        // Run the pilot deactivation command every day at midnight
+        $schedule->command('pilots:deactivate-overdue')->everyMinute();
     })->create();
